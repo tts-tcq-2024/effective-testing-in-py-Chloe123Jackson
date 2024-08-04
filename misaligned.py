9405220c880653b27428414d81594009add1a331
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-08-04 11:55:41 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,4 +1,4 @@
-
+import io
 def print_color_map():
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
@@ -7,7 +7,14 @@ def print_color_map():
             print(f'{i * 5 + j} | {major} | {minor}')
     return len(major_colors) * len(minor_colors)
 
+def fake_print_colour_map():
+    memory_buffer = io.StringIO()
+    print_color_map(memory_buffer)
+    return memory_buffer.getvalue()
+
 
 result = print_color_map()
+print_output=fake_print_colour_map()
+assert (print_output=2)
 assert(result == 25)
 print("All is well (maybe!)\n")
