3f64eb6f1072b741926f3214b6eb1df184e56dde
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-08-06 16:20:29 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -2,7 +2,8 @@ MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 table=[]
 
-from unittest.mock import Mock
+from unittest.mock import patch
+
 def print_color_map():
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
@@ -10,12 +11,19 @@ def print_color_map():
         for j, minor in enumerate(minor_colors):
             print(f'{i * 5 + j} | {major} | {minor}')
     return len(major_colors) * len(minor_colors)
-    
-def fake_print_colour_map():
-    mock = Mock()
+
+@patch("builtins.print")
+def fake_print_colour_map(mock_print):
+    print_colour_map()
     for row in table:
-        mock.print_colour_map()
-        mock.print_colour_map.assert_called_with('{:^2} | {:^6} | {:^6} |'.format(*row))
+        mock_print.assert_called_with('{:^2} | {:^6} | {:^6} |'.format(*row))
+        
+    
+# def fake_print_colour_map():
+#     mock = Mock()
+#     for row in table:
+#         mock.print_colour_map()
+#         mock.print_colour_map.assert_called_with('{:^2} | {:^6} | {:^6} |'.format(*row))
 
 def create_colour_code_table():
     pair_number=1
