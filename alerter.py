cfb0e23e7a021da028fc9dfa06e09e7452d5256a
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-08-07 15:47:47 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -14,17 +14,7 @@ def network_alert_stub(celcius):
 
 def alert_in_celcius(farenheit):
     celcius = (farenheit - 32) * 5 / 9
-
-def test_alert_failure():
-    network_alert_stub()
-    alert_in_celcius(400.5)
-    alert_in_celcius(303.6)
-    alert_in_celcius(20)
-    assert (alert_failure_count==1)
-    print(f'{alert_failure_count} alerts failed.')
-    print('All is well (maybe!)')
-    
-    
+    return celcius
     # returnCode = network_alert_stub(celcius)
     # if returnCode != 200:
     #     # non-ok response is not an error! Issues happen in life!
@@ -33,6 +23,18 @@ def test_alert_failure():
     #     # Add a test below to catch this bug. Alter the stub above, if needed.
     #     global alert_failure_count
     #     alert_failure_count += 0
+    
+
+def test_alert_failure():
+    network_alert_stub(alert_in_celcius(400.5))
+    network_alert_stub(alert_in_celcius(303.6))
+    network_alert_stub(alert_in_celcius(20))
+    assert (alert_failure_count==1)
+    print(f'{alert_failure_count} alerts failed.')
+    print('All is well (maybe!)')
+    
+    
+    
 
 
 
