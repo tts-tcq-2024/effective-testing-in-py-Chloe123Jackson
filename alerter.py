6f1964b21e32782917b344a2f86fbbd867081bae
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-08-08 11:21:26 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -1,12 +1,18 @@
 from unittest.mock import patch
 alert_failure_count = 0
+counter=0
 
 def network_alert_stub(celcius):
     print(f'ALERT: Temperature is {celcius} celcius')
     # Return 200 for ok
     # Return 500 for not-ok
     # stub always succeeds and returns 200
-    return 200   
+    global counter
+    counter+=1
+    if counter%3==0:
+        return 500
+    else:
+        return 200
 
 def alert_in_celcius(farenheit):
     celcius = (farenheit - 32) * 5 / 9
