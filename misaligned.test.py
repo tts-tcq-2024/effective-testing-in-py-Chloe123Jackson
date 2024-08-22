8c2672016c1b4c33957139684204a4bd6e07a6a5
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-08-22 23:48:54 +0530

    Update misaligned.test.py

--- misaligned.test.py
+++ misaligned.test.py
@@ -13,30 +13,8 @@ class Misaligned_test(unittest.TestCase):
             table_mock.append([i * 5 + j,major,minor])
     mock_print_colour_map.return_value=table_mock
     output_table=misaligned.print_color_map()
-    print("Output_table",output_table)
     ref_table=misaligned.create_colour_code_table()
-    print("Ref_table",ref_table)
-    self.assertEqual(ref_table,table_mock)
-  # assert misaligned.create_colour_code_table() == table_mock
-
-# class Misaligned_test(unittest.TestCase):
-#   @patch("builtins.print")
-#   def fake_print_colour_map(self,mock_print_colour_map):
-#     misaligned.print_color_map()
-#     mock_print_colour_map.assert_called_with("")
-#     table_mock=[]
-#     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-#     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-#     for i, major in enumerate(major_colors):
-#         for j, minor in enumerate(minor_colors):
-#             table_mock.append([i * 5 + j,major,minor])
-#     mock_print_colour_map.return_value=table_mock
-#     output_table=misaligned.print_color_map()
-#     print("Output_table",output_table)
-#     ref_table=create_colour_code_table()
-#     print("Ref_table",ref_table)
-#     self.assertEqual(ref_table,table_mock)
-  # assert misaligned.create_colour_code_table() == table_mock
+    self.assertEqual(ref_table,output_table)
 
 if __name__ == '__main__':
   unittest.main()
