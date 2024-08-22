4ed4f52eb4253dc490169f72cda1d3ec3e20c037
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-08-22 17:05:00 +0530

    Update misaligned.test.py

--- misaligned.test.py
+++ misaligned.test.py
@@ -4,8 +4,8 @@ from unittest.mock import patch
 
 class Misaligned_test(unittest.TestCase):
   @patch("misaligned.print_row")
-  def fake_print_colour_map(row_number,major,minor,mock_print_row):
-    global table_mock
-    table_mock.append([row_number,major,minor])
-    
+  def fake_print_colour_map(self,row_number,major,minor,mock_print_row):
+    # global table_mock
+    # table_mock.append([row_number,major,minor])
+    print(row_number,major)
   assert misaligned.create_colour_code_table() == table_mock
