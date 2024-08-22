8d7c69e17e44fed846d24fbbe433cb1fac313e5d
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-08-22 22:34:28 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -15,13 +15,6 @@ def print_color_map():
 def print_row(row_number,major,minor):
     print(f'{row_number} | {major} | {minor}')
     
-
-# @patch("misaligned.print_row")
-# def fake_print_colour_map(row_number,major,minor,mock_print_row):
-#     global table_moc
-#     table_mock.append([row_number,major,minor])
-#     mock_print_row.returnvalue = table_mock
-
 def create_colour_code_table():
     row=[]
     table=[]
@@ -36,27 +29,24 @@ def create_colour_code_table():
             row=[]
     return table
 
-# @patch("misaligned.print_color_map")
-# def fake_print_colour_map(self,mock_print_colour_map):
-#     table_mock=[]
-#     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-#     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-#     for i, major in enumerate(major_colors):
-#         for j, minor in enumerate(minor_colors):
-#             table_mock.append([i * 5 + j,major,minor])
-#     mock_print_colour_map.return_value=table_mock
-#     # output_table=misaligned.print_color_map()
-#     # print("Output_table",output_table)
-#     ref_table=create_colour_code_table()
-#     print("Ref_table",ref_table)
-#     self.assertEqual(ref_table,table_mock)
-#     # assert misaligned.create_colour_code_table() == table_mock
+@patch('misaligned.print_color_map')
+def fake_print_colour_map(self,mock_print_colour_map):
+    table_mock=[]
+    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    for i, major in enumerate(major_colors):
+        for j, minor in enumerate(minor_colors):
+            table_mock.append([i * 5 + j,major,minor])
+    mock_print_colour_map.return_value=table_mock
+    # output_table=misaligned.print_color_map()
+    # print("Output_table",output_table)
+    ref_table=create_colour_code_table()
+    print("Ref_table",ref_table)
+    self.assertEqual(ref_table,table_mock)
+    # assert misaligned.create_colour_code_table() == table_mock
 
 result = print_color_map()
 assert(result == 25)
 
- # create_colour_code_table()
-# print_color_map()
-# print(table_mock)
-# assert create_colour_code_table() == print_color_map()
+assert fake_print_colour_map() == True
 print("All is well (maybe!)\n")
