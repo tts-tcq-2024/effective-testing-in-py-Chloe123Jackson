82c91d60907fbb542a8c07af9157753f0056b601
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-08-22 21:22:46 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -36,6 +36,22 @@ def create_colour_code_table():
             row=[]
     return table
 
+  @patch("misaligned.print_color_map")
+  def fake_print_colour_map(self,mock_print_colour_map):
+    table_mock=[]
+    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    for i, major in enumerate(major_colors):
+        for j, minor in enumerate(minor_colors):
+            table_mock.append([i * 5 + j,major,minor])
+    mock_print_colour_map.return_value=table_mock
+    # output_table=misaligned.print_color_map()
+    # print("Output_table",output_table)
+    ref_table=misaligned.create_colour_code_table()
+    print("Ref_table",ref_table)
+    self.assertEqual(ref_table,table_mock)
+  # assert misaligned.create_colour_code_table() == table_mock
+
 # result = print_color_map()
 # assert(result == 25)
 # create_colour_code_table()
