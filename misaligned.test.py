85efdb366fb48c7a1bd894bbb01a62db14e7cd38
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-08-22 20:25:01 +0530

    Update misaligned.test.py

--- misaligned.test.py
+++ misaligned.test.py
@@ -8,8 +8,8 @@ class Misaligned_test(unittest.TestCase):
     global table_mock
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-    for i, major in enumerate(MAJOR_COLORS):
-        for j, minor in enumerate(MINOR_COLORS):
+    for i, major in enumerate(major_colors):
+        for j, minor in enumerate(minor_colors):
             table_mock.append([i * 5 + j,major,minor])
     mock_print_colour_map.returnvalue=table_mock
   assert misaligned.create_colour_code_table() == table_mock
